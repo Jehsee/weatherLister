5c938b980101b7254df990bf16284d71d400d7de
Jesse <jessehwang86@yahoo.com>
2017-10-24 18:39:33 -0400

    finished app

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -3,7 +3,8 @@ import { FETCH_WEATHER } from '../actions/index';
 export default function(state = [], action) {
 	switch(action.type) {
 	case FETCH_WEATHER:
-		return state.concat([action.payload.data])
+		// the below syntax is equal to: return state.concat([action.payload.data])
+		return [action.payload.data, ...state]
 	}
 
 	return state;
